18792e02f0eea2470e63f5bb9a555423c9984b84
Tamar Regev <tamarr@jazz.soc.huji.ac.il>
2016-01-05 17:48:56 +0200

    all updates by Tamar

--- rerp.m
+++ rerp.m
@@ -1,4 +1,5 @@
-function [ glm_estimates, unique_event_types ] = rerp( input_data, event_latencies, event_types, window_length )
+function [ glm_estimates, design_matrix ] = rerp( input_data, varargin)
+%
 %RERP performs a linear regression based analysis of event-related data.
 % 
 % Usage: 
@@ -19,47 +20,117 @@ function [ glm_estimates, unique_event_types ] = rerp( input_data, event_latenci
 %    considerations. Psychophysiology, 52, pp.169–181.
 % 
 % Written by Tal Golan and Edden Gerber, Jan. 2016 
-%
+% - Tamar Regev - 5/1/2016 
+%   - added warning and fix if last event window exceeds data length
+%   - added artifact rejection 
+%   - optionally accept matrix X as input and do not calculate it 
+%     --> input ossibilities are
+%       rerp( input_data, design_matrix)
+%       rerp( input_data, design_matrix, artifact_indexes)
+%       rerp( input_data, event_latencies, event_types, window_length)
+%       rerp( input_data, event_latencies, event_types, window_length, artifact_indexes )
+
+tictot = tic;
+
+switch length(varargin)
+    case 0
+        error('not enough input arguments')
+    case 1
+        Xexists = true;
+        design_matrix = varargin{1};
+        event_types = design_matrix.event_types;
+        event_latencies = design_matrix.event_latencies;
+        window_length = design_matrix.window_length;
+    case 2
+        Xexists = true;
+        design_matrix = varargin{1};
+        artifact_indexes = varargin{2};
+        event_types = design_matrix.event_types; 
+        event_latencies = design_matrix.event_latencies;
+        window_length = design_matrix.window_length;
+    case 3
+        Xexists = false;
+        event_latencies = varargin{1};
+        event_types = varargin{2};
+        window_length = varargin{3};
+    case 4
+        Xexists = false;
+        event_latencies = varargin{1};
+        event_types = varargin{2};
+        window_length = varargin{3};
+        artifact_indexes = varargin{4};
+end
 
 data_len = length(input_data);
 unique_event_types = unique(event_types);
 num_event_types = length(unique_event_types);
 
-i = nan(length(event_latencies)*window_length,1);
-j = nan(length(event_latencies)*window_length,1);
-
-n = 1;
-for ii = 1:num_event_types
-    current_event_type = unique_event_types(ii);
-    num_events = sum(event_types==current_event_type);
-    
-    pulse_latencies=bsxfun(@plus,event_latencies(event_types==current_event_type),0:window_length-1); % (nEvents x nLags)
-    pulse_predictors=repmat(1:window_length,numel(event_latencies(event_types==current_event_type)),1); % (nEvents x nLags)
-    
-    i(n:n+num_events*window_length-1) = pulse_latencies(:);
-    j(n:n+num_events*window_length-1) = pulse_predictors(:) + window_length*(ii-1);
-    
-    n = n + num_events*window_length;
+% Check if last event windows exceed data length. Warn and fix by eliminating it :
+nEvents_exceeding = sum(event_latencies+window_length-1 >= data_len);
+if nEvents_exceeding
+    warning([num2str(nEvents_exceeding) 'last events exceed data length'])
+    warning('eliminating events from analysis')
+    event_latencies(end+1-nEvents_exceeding:end)=[];
 end
 
-% add checks for minimal and maximal timepoints of the pulses
+%calculate design matrix, if not provided as input
+if Xexists
+    disp('Design matrix provided as input')
+    X = design_matrix.matrix;
+    window_length = design_matrix.window_length;
+    num_event_types = design_matrix.num_event_types;
+else
+    disp(['Calculating design matrix...'])
+    
+    i = nan(length(event_latencies)*window_length,1);
+    j = nan(length(event_latencies)*window_length,1);
 
-% build a sparse design matrix
-X = sparse(i,j,1,data_len,window_length*num_event_types);
+    n = 1;
+    for ii = 1:num_event_types
+        current_event_type = unique_event_types(ii);
+        num_events = sum(event_types==current_event_type);
 
-% add a constant predictor
-X = cat(2,X,sparse(ones(data_len,1)));
+        pulse_latencies=bsxfun(@plus,event_latencies(event_types==current_event_type),0:window_length-1); % (nEvents x nLags)
+        pulse_predictors=repmat(1:window_length,numel(event_latencies(event_types==current_event_type)),1); % (nEvents x nLags)
 
-%TODO - add artifact rejection here
+        i(n:n+num_events*window_length-1) = pulse_latencies(:);
+        j(n:n+num_events*window_length-1) = pulse_predictors(:) + window_length*(ii-1);
+
+        n = n + num_events*window_length;
+
+    end
+    
+    % build a sparse design matrix
+    X = sparse(i,j,1,data_len,window_length*num_event_types);
+
+    % add a constant predictor
+    X = cat(2,X,sparse(ones(data_len,1)));
+    disp(['Done'])
+    
+    %assign design matrix struct
+    design_matrix = struct;
+    design_matrix.matrix = X;
+    design_matrix.event_types = event_types;
+    design_matrix.unique_event_types = unique_event_types;
+    design_matrix.window_length = window_length;
+    design_matrix.num_event_types = num_event_types;
+    design_matrix.event_latencies = event_latencies;
+end
+
+%reject artifacts
+X(artifact_indexes,:) = [];
+input_data(artifact_indexes,:) = [];
 
 % ordinary least squares: solve observedTimecourse=X*b;
-b=X\input_data;
+disp('running regression...');tic;
+b = X\input_data;
 
 % get predicted values
 glm_estimates = zeros(window_length,num_event_types);
 for ii = 1:num_event_types
     glm_estimates(:,ii)=b((ii-1)*window_length+1:ii*window_length);
 end
-
+disp(['Done in ' num2str(toc) ' sec.'])
+disp(['Done all in ' num2str(toc(tictot)) ' sec.'])
 end
 
